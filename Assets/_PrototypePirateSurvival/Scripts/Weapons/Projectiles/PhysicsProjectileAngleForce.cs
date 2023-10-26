ca4ddb3d6da766891616158794789a693879aaaf
Thinh <dqthinh69752@gmail.com>
2023-10-27 00:22:44 +0700

    Test formula calculating distance travelled by projectile [WIP]

--- Assets/_PrototypePirateSurvival/Scripts/Weapons/Projectiles/PhysicsProjectileAngleForce.cs
+++ Assets/_PrototypePirateSurvival/Scripts/Weapons/Projectiles/PhysicsProjectileAngleForce.cs
@@ -23,15 +23,47 @@ public class PhysicsProjectileAngleForce : PhysicsProjectile
 		}
 
 		// TEST
+		// Rough estimate: since start at initial height h0, the actual distance will be longer
 		// x = (v0^2 * sin(2a)) / g
-		var initialVelocity = InitialForce * newDirection.y / _rigidBody.mass;
+		var initialVelocity = (InitialForce / _rigidBody.mass) * Time.fixedDeltaTime;
 		Debug.Log($"Velocity: {initialVelocity}");
 
-		var weaponAim = _weapon.gameObject.MMGetComponentNoAlloc<WeaponAim3D>();
-		if (weaponAim != default)
-        {
-			var angle = Vector3.Angle(weaponAim.CurrentAim, newDirection);
-			Debug.Log($"Angle: {angle}");		// WRONG!!!
-        }
+		var tempDirection = newDirection;
+		var projectedDirection = tempDirection;
+		projectedDirection.y = 0;
+
+		var angle = Vector3.Angle(tempDirection, projectedDirection);
+		Debug.Log($"Angle: {angle}");
+
+		var gravity = Physics.gravity;
+		var g = Physics.gravity.magnitude;
+		Debug.Log($"g: {gravity}");
+
+		var maxHeightDelta = initialVelocity * initialVelocity / (2 * g);
+		Debug.Log($"Max height: {maxHeightDelta}");
+
+		var maxDistanceDelta = initialVelocity * initialVelocity * Mathf.Sin(2 * Mathf.Deg2Rad * angle) / g;
+		Debug.Log($"Max distance: {maxDistanceDelta}");
+
+		var initialDistanceDelta = (transform.position.y + 0.5) / Mathf.Tan(Mathf.Deg2Rad * angle);
+		Debug.Log($"Initial distance: {initialDistanceDelta}");
+
+		var finalRadius = 0.6f + maxDistanceDelta + initialDistanceDelta;
+		Debug.Log($"Final redius: {finalRadius}");
+
+
+		var fireZone = GameObject.Find("FireZone");
+		if (fireZone != default)
+		{
+			var circle = fireZone.GetComponentInChildren<SpriteRenderer>();
+			Debug.Log($"Bounds: {circle.bounds.extents}");
+		}
 	}
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+
+		Debug.Log($"Position: {transform.position}");
+    }
 }
