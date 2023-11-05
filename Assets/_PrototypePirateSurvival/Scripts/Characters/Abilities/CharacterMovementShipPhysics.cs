0fdbd996b05925069b7565564ccf91a17f22ee1d
Thinh <dqthinh69752@gmail.com>
2023-11-05 23:08:31 +0700

    Improve physics-like ship movement control [WIP]

--- Assets/_PrototypePirateSurvival/Scripts/Characters/Abilities/CharacterMovementShipPhysics.cs
+++ Assets/_PrototypePirateSurvival/Scripts/Characters/Abilities/CharacterMovementShipPhysics.cs
@@ -1,5 +1,7 @@
 using MoreMountains.Tools;
 using MoreMountains.TopDownEngine;
+using System.Security.Cryptography;
+using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 
 public class CharacterMovementShipPhysics : CharacterMovement
@@ -10,105 +12,61 @@ public class CharacterMovementShipPhysics : CharacterMovement
     [Tooltip("whether to apply modifications made for real-life ship physics simulation")]
     public bool IsAppliedShipPhysics;
 
-    // the threshold angle (in degrees) between successive movement/input vectors, above which inertia will be applied
-    //[Tooltip("the threshold angle (in degrees) between successive movement/input vectors, above which inertia will be applied")]
-    //public float ThresholdAngle = 0f;
-
-    // whether to apply inertia for movement vectors
-    [Tooltip("whether to apply inertia for movement vectors")]
-    public bool IsMovementInertia;
-
     // the inertia resistance against the abrupt change in movement direction (higher means longer to adapt to new movement)
-    [MMCondition(nameof(IsMovementInertia), Negative = false)]
     [Tooltip("the inertia resistance against the abrupt change in movement direction (higher means longer to adapt to new movement)")]
     [Min(1f)]
     public float MovementInertiaResistance = 2f;
 
-    // the deceleration to gradually cancel out the movement inertia resistance
-    [MMCondition(nameof(IsMovementInertia), Negative = false)]
-    [Tooltip("the deceleration to gradually cancel out the movement inertia resistance")]
-    public float MovementInertiaDeceleration = 0.2f;
+    // the inertia speed in case of abrupt change in movement direction (higher value means slower transition to new movement direction)
+    [Tooltip("the inertia speed in case of abrupt change in movement direction (higher value means slower transition to new movement direction)")]
+    public Vector2 MovementInertiaSpeed = new(2f, 2f);
 
-    // the limit to full stop the movement inertia resistance
-    [MMCondition(nameof(IsMovementInertia), Negative = false)]
-    [Tooltip("the limit to full stop the movement inertia resistance")]
-    public float LimitToCancelInertia = 0.005f;
+    // the deceleration to gradually cancel out the movement inertia
+    [Tooltip("the deceleration to gradually cancel out the movement inertia")]
+    public Vector2 MovementInertiaDeceleration = new(0.2f, 0.2f);
 
-
-    // whether to apply inertia for input vectors
-    [Tooltip("whether to apply inertia for input vectors")]
-    public bool IsInputInertia;
-
-    // the inertia resistance against the abrupt change in input direction (higher means more resistance)
-    [MMCondition(nameof(IsInputInertia), Negative = false)]
-    [Tooltip("the inertia resistance against the abrupt change in input direction (higher means more resistance)")]
-    [Min(1f)]
-    public float InputInertiaResistance = 2f;
-
-    // the (de)acceleration to gradually cancel out the input inertia resistance
-    [MMCondition(nameof(IsInputInertia), Negative = false)]
-    [Tooltip("the (de)acceleration to gradually cancel out the input inertia resistance")]
-    public float InputInertiaDeacceleration = 0.2f;
-
-    // the limit to full stop the input inertia resistance
-    [MMCondition(nameof(IsInputInertia), Negative = false)]
-    [Tooltip("the limit to full stop the input inertia resistance")]
-    public float LimitToCancelInputInertia = 0.005f;
+    // the limit to cancel the movement inertia resistance
+    [Tooltip("the limit to cancel the movement inertia resistance")]
+    public Vector2 LimitToCancelInertia = new(0.1f, 0.1f);
 
 
-    protected Vector2 _previousInput;
-    protected Vector2 _newInput;
+    // whether to apply extra modifications in case of large angles
+    [Tooltip("whether to apply extra modifications in case of large angles")]
+    public bool IsLargeAngleModified;
+
+    // the threshold angle (in degrees) between successive movement/input vectors, above which movement vector modifications will be applied
+    [MMCondition(nameof(IsLargeAngleModified), Hidden = true)]
+    [Tooltip("the threshold angle (in degrees) between successive movement/input vectors, above which extra intertia modifications will be applied")]
+    public float ThresholdAngle = 0f;
+
+    // the modifiers for the movement vector increase in case of large angles
+    [MMCondition(nameof(IsLargeAngleModified), Hidden = true)]
+    [Tooltip("the modifiers for the movement vector increase in case of large angles")]
+    public Vector2 ModifierIncreaseMovement = new(1f, 1f);
+
+    // the speed at which movement vector changes in case of large angles
+    [MMCondition(nameof(IsLargeAngleModified), Hidden = true)]
+    [Tooltip("the speed at which movement vector changes in case of large angles")]
+    public Vector2 SpecializedMovementInertiaSpeed = new(1f, 1f);
+
+    // the deceleration to gradually cancel out the specialized movement inertia
+    [MMCondition(nameof(IsLargeAngleModified), Hidden = true)]
+    [Tooltip("the deceleration to gradually cancel out the specialized movement inertia")]
+    public Vector2 SpecializedMovementInertiaDeceleration = new(0.2f, 0.2f);
+
+
+    // whether to clamp magnitude of movement vector
+    [Tooltip("whether to clamp magnitude of movement vector")]
+    public bool IsClampMagnitude;
+
+    // TODO: clamp magnitude movement vector
+
 
     protected Vector3 _previousMovementVector;
     protected Vector3 _newMovementVector;
 
-    protected CharacterOrientation3D _characterOrientation;
-
-
-    protected override void Initialization()
-    {
-        base.Initialization();
-
-        _characterOrientation = _character.FindAbility<CharacterOrientation3D>();
-    }
-
-    Vector3 _newDirection;
-    Vector3 _previousDirection;
-    Vector3 _currentDirection;
-
-    // TODO: lerp horizontal input (and do the same for CharacterOrientation3D)
-    float _prevHorizontalInput = 0f;
-    float _prevVerticalInput = 0f;
-    Vector2 _currentInput2 = Vector2.zero;
-
-    protected override void HandleInput()
-    {
-        if (ScriptDrivenInput || !InputAuthorized)
-        {
-            return;
-        }
-
-        var prevInput = new Vector2(_prevHorizontalInput, _prevVerticalInput);
-        _currentInput2 = new Vector2(_horizontalInput, _verticalInput);
-
-        var deltaAngle = Vector2.SignedAngle(prevInput, _currentInput2);
-        var angleToRotate = Mathf.Lerp(0, deltaAngle, Time.deltaTime);
-        Debug.Log($"Angle: {angleToRotate}");
-
-        if (Mathf.Abs(deltaAngle) >= 90f)
-        {
-            _currentInput2 = Quaternion.AngleAxis(45f * Mathf.Sign(angleToRotate), transform.up) * _currentInput2;
-            _horizontalInput = _currentInput2.x;
-            _verticalInput = _currentInput2.y;
-
-            _prevHorizontalInput = _horizontalInput;
-            _prevVerticalInput = _verticalInput;
-        }
-
-        // TODO: next....
-
-        base.HandleInput();
-    }
+    protected Vector2 _inertiaSpeed = Vector2.zero;
+    protected Vector2 _specializedInertiaSpeed = Vector2.zero;
 
 
     protected override void SetMovement()
@@ -119,91 +77,89 @@ public class CharacterMovementShipPhysics : CharacterMovement
             return;
         }
 
-        if (IsMovementInertia)
+        _previousMovementVector = _movementVector;
+
+        // Reset
+        if (_previousMovementVector.x == 0)
         {
-            _previousMovementVector = _movementVector;
+            _inertiaSpeed.x = MovementInertiaSpeed.x;
+        }
 
-            base.SetMovement();
+        if (_previousMovementVector.y == 0)
+        {
+            _inertiaSpeed.y = MovementInertiaSpeed.y;
+        }
 
-            //if (Vector3.Angle(_previousMovementVector, _movementVector) < ThresholdAngle)
-            //{
-            //    return;
-            //}
 
-            bool isCancelMovementX = Mathf.Approximately(_horizontalMovement, 0f);
-            bool isCancelMovementZ = Mathf.Approximately(_verticalMovement, 0f);
+        base.SetMovement();
 
-            var baseInertiaSpeed = 1f / MovementInertiaResistance;
-            if (_movementVector.magnitude > _previousMovementVector.magnitude)
+        bool isCancelMovementX = Mathf.Approximately(_horizontalMovement, 0f);
+        bool isCancelMovementZ = Mathf.Approximately(_verticalMovement, 0f);
+
+        var vector3CurrentInput = new Vector3(_currentInput.x, 0, _currentInput.y);
+        vector3CurrentInput = Vector3.ClampMagnitude(vector3CurrentInput, _previousMovementVector.magnitude);
+
+        var angle = Vector3.Angle(vector3CurrentInput, _previousMovementVector);
+
+        // TODO: check how to turn ship from stationary (or near zero movement)
+        var isBackwardFromStationary = _previousMovementVector.magnitude <= 0.5f && _currentInput.magnitude >= 0.5f;
+
+        if (IsLargeAngleModified && (angle >= ThresholdAngle || isBackwardFromStationary))
+        {
+            //Debug.LogError($"Angle: {angle}");
+
+            var modifiedMovementVector = _movementVector;
+
+            var increaseMovementValue = Mathf.Abs(_previousMovementVector.magnitude);
+            var movementModifierVector = new Vector3(ModifierIncreaseMovement.x, 0, ModifierIncreaseMovement.y);
+            var modifierAngle = Vector3.SignedAngle(transform.forward, _previousMovementVector, transform.up);
+
+            movementModifierVector = Quaternion.AngleAxis(modifierAngle, transform.up) * movementModifierVector;
+
+            //Debug.Log($"Modifers: {movementModifierVector}");
+
+            modifiedMovementVector.x += increaseMovementValue * movementModifierVector.x;
+            modifiedMovementVector.z += increaseMovementValue * movementModifierVector.z;
+            // TODO: clamp magnitude only, but keep the sign
+
+            //Debug.Log($"Modified: {modifiedMovementVector}");
+
+            var acceleration = SpecializedMovementInertiaDeceleration.x;
+            _specializedInertiaSpeed.x = Mathf.Lerp(_specializedInertiaSpeed.x, _specializedInertiaSpeed.x + (1 + acceleration), acceleration * Time.deltaTime);
+
+            acceleration = SpecializedMovementInertiaDeceleration.y;
+            _specializedInertiaSpeed.y = Mathf.Lerp(_specializedInertiaSpeed.y, _specializedInertiaSpeed.y + (1 + acceleration), acceleration * Time.deltaTime);
+
+
+            if (Mathf.Abs(_previousMovementVector.x) <= 0.5f)
             {
-                baseInertiaSpeed = 1f / Mathf.Clamp(MovementInertiaResistance * 0.5f, 1, MovementInertiaResistance);
+                _previousMovementVector.x = 0.5f;
             }
 
-            // ===
-            //// TODO: calculate x movement to rotate a little bit sideway
-            //if (_characterOrientation != default && _characterOrientation.ShouldRotateToFaceMovementDirection)
-            //{
-            //    var rotatingModel = (_characterOrientation.MovementRotatingModel != default) ? _characterOrientation.MovementRotatingModel : _model;
+            if (Mathf.Abs(_previousMovementVector.z) <= 0.2f)
+            {
+                _previousMovementVector.z = 0.2f;
+            }
 
-            //    // TODO: more conditions
-
-            //    _previousDirection = _currentDirection;
-            //    _currentDirection = _characterOrientation.ModelDirection; ;
-
-            //    var rotation = Quaternion.AngleAxis(Vector3.SignedAngle(_previousDirection, _currentDirection, transform.up) * 5f / 360f, transform.up);
-
-            //    var tempRotation = Quaternion.LookRotation(_currentDirection);
-            //    var newMovementQuaternion = Quaternion.Slerp(rotatingModel.transform.rotation, tempRotation, Time.deltaTime * _characterOrientation.RotateToFaceMovementDirectionSpeed);
-
-            //    var newDirection = newMovementQuaternion * _previousDirection;
-            //    //_newDirection = rotation * previousDirection;
-            //    Debug.Log($"Rotation: {newMovementQuaternion.eulerAngles.y}");
-
-            //    _movementVector.x += 5 * _newDirection.x;
-            //    _movementVector.z += 5 * _newDirection.z;
-            //}
-            // ===
-
-
-
-            var speed = Mathf.Lerp(baseInertiaSpeed, baseInertiaSpeed * (1 + MovementInertiaDeceleration), MovementInertiaDeceleration * Time.deltaTime);
-            //Debug.Log($"Applied speed: {speed}");
-
-            _newMovementVector.x = Mathf.Lerp(_previousMovementVector.x, _movementVector.x, speed * Time.deltaTime);
-            _newMovementVector.z = Mathf.Lerp(_previousMovementVector.z, _movementVector.z, speed * Time.deltaTime);
-
-            _movementVector.x = isCancelMovementX && Mathf.Abs(_newMovementVector.x) < LimitToCancelInertia ? _movementVector.x : _newMovementVector.x;
-            _movementVector.z = isCancelMovementZ && Mathf.Abs(_newMovementVector.z) < LimitToCancelInertia ? _movementVector.z : _newMovementVector.z;
-
-            _controller.SetMovement(_movementVector);
+            _newMovementVector.x = Mathf.Lerp(_previousMovementVector.x, modifiedMovementVector.x, _specializedInertiaSpeed.x * Time.deltaTime);
+            _newMovementVector.z = Mathf.Lerp(_previousMovementVector.z, modifiedMovementVector.z, _specializedInertiaSpeed.y * Time.deltaTime);
         }
-        else if (IsInputInertia)
+        else
         {
-            _previousInput = _currentInput;
+            // Reset
+            _specializedInertiaSpeed = SpecializedMovementInertiaSpeed;
 
-            _newInput.x = _horizontalMovement;
-            _newInput.y = _verticalMovement;
+            _inertiaSpeed.x = Mathf.Lerp(_inertiaSpeed.x, _inertiaSpeed.x * (1 + MovementInertiaDeceleration.x), MovementInertiaDeceleration.x * Time.deltaTime);
+            _inertiaSpeed.y = Mathf.Lerp(_inertiaSpeed.y, _inertiaSpeed.y * (1 + MovementInertiaDeceleration.y), MovementInertiaDeceleration.y * Time.deltaTime);
 
-            //if (Vector2.Angle(_newInput, _previousInput) < ThresholdAngle)
-            //{
-            //    base.SetMovement();
-            //    return;
-            //}
-
-            bool isCancelInputX = Mathf.Approximately(_newInput.x, 0f);
-            bool isCancelInputY = Mathf.Approximately(_newInput.y, 0f);
-
-            var baseInertiaSpeed = 1f / InputInertiaResistance;
-            var speed = Mathf.Lerp(baseInertiaSpeed, baseInertiaSpeed * (1 + InputInertiaDeacceleration), InputInertiaDeacceleration * Time.deltaTime);
-            //Debug.Log($"Applied speed: {speed}");
-
-            _newInput = Vector2.Lerp(_previousInput, _newInput, speed * Time.deltaTime);
-
-            _horizontalMovement = isCancelInputX && Mathf.Abs(_newInput.x) < LimitToCancelInertia ? 0f : _newInput.x;
-            _verticalMovement = isCancelInputY && Mathf.Abs(_newInput.y) < LimitToCancelInertia ? 0 : _newInput.y;
-
-            base.SetMovement();
+            _newMovementVector.x = Mathf.Lerp(_previousMovementVector.x, _movementVector.x, _inertiaSpeed.x * Time.deltaTime);
+            _newMovementVector.z = Mathf.Lerp(_previousMovementVector.z, _movementVector.z, _inertiaSpeed.y * Time.deltaTime);
         }
+
+        _movementVector.x = isCancelMovementX && Mathf.Abs(_newMovementVector.x) < LimitToCancelInertia.x ? _movementVector.x : _newMovementVector.x;
+        _movementVector.z = isCancelMovementZ && Mathf.Abs(_newMovementVector.z) < LimitToCancelInertia.y ? _movementVector.z : _newMovementVector.z;
+
+        _controller.SetMovement(_movementVector);
     }
 
     private void OnDrawGizmos()
@@ -214,8 +170,8 @@ public class CharacterMovementShipPhysics : CharacterMovement
             Gizmos.color = Color.blue;
             Gizmos.DrawLine(origin, origin + 3 * _movementVector);
 
-            Gizmos.color = Color.red;
-            Gizmos.DrawLine(origin, origin + 3 * _characterOrientation.ModelDirection);
+            //Gizmos.color = Color.red;
+            //Gizmos.DrawLine(origin, origin + 3 * _characterOrientation.ModelDirection);
         }
     }
 }
